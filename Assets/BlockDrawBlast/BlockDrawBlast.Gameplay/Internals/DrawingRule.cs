79aa465af4d8e848094eb54a4bbf6fb813b45fb9
napoleloc <napoleloc@gmail.com>
2025-07-14 16:13:10 +0700

    Add `DrawingContext` implementation and enhance `DrawingRule` with diagonal drawing support

--- Assets/BlockDrawBlast/BlockDrawBlast.Gameplay/Internals/DrawingRule.cs
+++ Assets/BlockDrawBlast/BlockDrawBlast.Gameplay/Internals/DrawingRule.cs
@@ -4,6 +4,7 @@
     {
         public DrawingPattern Pattern { get; init; }
         public bool RequiresConnectedPath { get; init; }
+        public bool AllowDiagonalDrawing { get; init; }
     }
 }
 
